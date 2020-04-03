17b3662c156f3e1cd026e5c38c466187717f36e4
Jessica Lam <jemlam@jessicas-mbp.lan>
2020-04-02 17:29:54 -0700

    finished endpoints for account-router

---
+++ accounts/account-router.js
@@ -0,0 +1,86 @@
+const express = require("express");
+
+const db = require("../data/dbConfig");
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    db.select("*")
+    .from("accounts")
+    .then(rows => {
+        res.status(200).json({data: rows});
+    })
+    .catch(err => {
+        res.status(500).json({message: "Sorry, ran into an error."})
+    });
+});
+//works on Postman
+
+router.get('/:id', (req, res) => {
+    db("accounts")
+    .where({id: req.params.id})
+    .first()
+    .then(account => {
+        if (account) {
+            res.status(200).json({data: account})
+        } else {
+            res.status(404).json({message: "Account cannot be found."})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: "Sorry, ran into an error."})
+    })
+})
+    
+//works on Postman
+
+router.post('/', (req, res) => {
+    db("accounts")
+    .insert(req.body, "id")
+    .then(ids => {
+        res.status(201).json({results: ids})
+    })
+    .catch(err => {
+        res.status(500).json({message: "Sorry, ran into an error."});
+    });
+});
+//works on Postman
+
+
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+
+    db("accounts")
+    .where({id: req.params.id})
+    .update(changes)
+    .then(count => {
+        if (count > 0) {
+            res.status(200).json({message: "Account updated successfully."});
+        } else {
+            res.status(404).json({message: "Account cannot be found."});
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: "Sorry, ran into an error."});
+    });
+});
+//worked on Postman
+
+router.delete('/:id', (req, res) => {
+    db("accounts")
+    .where({id: req.params.id})
+    .del()
+    .then(count => {
+        if (count > 0) {
+            res.status(200).json({message: "Account deleted successfully."});
+        } else {
+            res.status(404).json({message: "Account cannot be found."});
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: "Sorry, ran into an error."})
+    })
+})
+//works on Postman
+
+module.exports = router;
